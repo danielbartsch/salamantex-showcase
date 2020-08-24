800ec1b26118cd7e615e8d9e93708ce0d7f9e3df
danielbartsch <daniel.bartsch@adverity.com>
2020-08-24 20:25:46 +0200

    define typescript types

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
 import React from "react"
 
+type CryptoCurrencyType = "bitcoin" | "ethereum"
+
+type CryptoCurrency = {
+  type: CryptoCurrencyType
+  walletId: string
+  balance: number
+  maxTransactionAmount: number
+}
+
+type UserId = string
+type User = {
+  id: UserId
+  name: string
+  description: string
+  email: string
+  currencies: Array<CryptoCurrency>
+}
+
+type TransactionId = string
+type Transaction = {
+  id: TransactionId
+  amount: number
+  type: CryptoCurrencyType
+  sourceUserId: UserId
+  targetUserId: UserId
+  created: number
+  processed: number | null
+  state: "invalid" | "processed" | null
+}
+
 function App() {
   return <>Begin</>
 }
