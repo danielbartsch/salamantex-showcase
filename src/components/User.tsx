ce3c2e46dab09af68ee38a41758a47b356379393
danielbartsch <daniel.bartsch@adverity.com>
2020-08-27 22:45:40 +0200

    made transaction list a little easier to look at

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -111,11 +111,26 @@ const TransactionList = ({ user }: { user: UserType }) => {
   return (
     <>
       {transactions.length > 0 ? (
-        <ul>
+        <div
+          style={{
+            backgroundColor: "#fefeff",
+            borderTop: "4px solid #eef",
+            borderRight: "1px solid #dde",
+            borderLeft: "1px solid #dde",
+            borderBottom: "1px solid #eef",
+            borderRadius: 2,
+            boxShadow: "0px 1px 5px #0013",
+            padding: 4,
+          }}
+        >
           {transactions.map((transaction) => (
-            <Transaction key={transaction.id} transaction={transaction} />
+            <Transaction
+              key={transaction.id}
+              meId={user.id}
+              transaction={transaction}
+            />
           ))}
-        </ul>
+        </div>
       ) : (
         "No transactions found..."
       )}
@@ -123,14 +138,77 @@ const TransactionList = ({ user }: { user: UserType }) => {
   )
 }
 
-const Transaction = ({ transaction }: { transaction: TransactionType }) => {
+const Arrow = ({
+  direction = "left",
+  color,
+}: {
+  direction?: "left" | "right"
+  color: string
+}) => {
+  const path = (
+    <path d="m0,6 l6,6 l0,-2 l18,0 l0,-8 l-18,0 l0,-2z" fill={color} />
+  )
   return (
-    <li>
-      {transaction.state === "processed" ? "Successfull:" : null}
-      {transaction.state === "invalid" ? "Failed:" : null}
-      <UserRepresentation id={transaction.sourceUserId} /> sent{" "}
-      {transaction.amount} <CurrencyType type={transaction.type} /> to{" "}
-      <UserRepresentation id={transaction.targetUserId} />
-    </li>
+    <svg viewBox="0 0 24 12" width="24" height="10">
+      {direction === "left" ? (
+        path
+      ) : (
+        <g transform="scale(-1) translate(-24,-12)">{path}</g>
+      )}
+    </svg>
+  )
+}
+
+const TransactionState = ({ state }: { state: TransactionType["state"] }) => {
+  switch (state) {
+    case "processed":
+      return (
+        <svg viewBox="0 0 12 12" width="12" height="12">
+          <path d="m9,1 l2,2 l-7,7 l-4,-4 l2,-2 l2,2z" fill="#4a4" />
+        </svg>
+      )
+    case "invalid":
+      return (
+        <svg viewBox="0 0 12 12" width="12" height="12">
+          <path
+            d="m2,0 l-2,2 l10,10 l2,-2 l-10,-10z M12,2 l-2,-2 l-10,10 l2,2 l-10,10z"
+            fill="#a44"
+          />
+        </svg>
+      )
+    default:
+      return (
+        <svg viewBox="0 0 12 12" width="12" height="12" className="rotate">
+          <circle r="2" cx="6" cy="2" fill="#aaa" />
+          <circle r="2" cx="6" cy="10" fill="#aaa" />
+        </svg>
+      )
+  }
+}
+
+const Transaction = ({
+  transaction,
+  meId,
+}: {
+  transaction: TransactionType
+  meId: UserType["id"]
+}) => {
+  return (
+    <div>
+      <TransactionState state={transaction.state} />
+      {meId === transaction.sourceUserId ? (
+        <>
+          <Arrow direction="right" color="#a44" /> {transaction.amount}{" "}
+          <CurrencyType type={transaction.type} />{" "}
+          <UserRepresentation id={transaction.targetUserId} />
+        </>
+      ) : (
+        <>
+          <Arrow color="#4a4" /> {transaction.amount}{" "}
+          <CurrencyType type={transaction.type} />{" "}
+          <UserRepresentation id={transaction.sourceUserId} />
+        </>
+      )}
+    </div>
   )
 }
